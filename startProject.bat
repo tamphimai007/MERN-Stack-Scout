24c9b56b46c7615753ef489c4940fbf2fd22e6ec
tamphimai007 <74285470+tamphimai007@users.noreply.github.com>
2021-07-31 20:22:46 +0700

    Add files via upload

---
+++ startProject.bat
@@ -0,0 +1,7 @@
+start /d "C:\Users\ISAN-PC\Desktop\project\5 scout\client" npm start
+
+start /d "C:\Users\ISAN-PC\Desktop\project\5 scout\server" npm start
+
+cd C:\Users\ISAN-PC\Desktop\project\5 scout
+code .
+
